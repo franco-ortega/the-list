82de4ee74ad139e8b1e780289a06b059d4b3216a
Franco Ortega <francopdx@gmail.com>
2024-10-07 22:37:02 -0700

    add headings to home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,15 @@ import styles from './page.module.css';
 export default function Home() {
 	return (
 		<div className={styles.page}>
-			<main className={styles.main}>Hello, Main</main>
-			<footer className={styles.footer}>Hey, Footer</footer>
+			<header>
+				<h1>Hello, List</h1>
+			</header>
+			<main className={styles.main}>
+				<h2>Hello, Main</h2>
+			</main>
+			<footer className={styles.footer}>
+				<h3>Hey, Footer</h3>
+			</footer>
 		</div>
 	);
 }
