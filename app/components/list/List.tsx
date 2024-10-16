841cdefa9eee1fa8b4816df7973290892ea11520
Franco Ortega <francopdx@gmail.com>
2024-10-15 17:01:23 -0700

    create ListType to use ListItemType array

--- app/components/list/List.tsx
+++ app/components/list/List.tsx
@@ -1,8 +1,8 @@
-import { ListItemType } from '@/app/utils/types';
+import { ListType } from '@/app/utils/types';
 import ListItem from '../listItem/ListItem';
 
 type Props = {
-	list: ListItemType[];
+	list: ListType[];
 };
 
 export default function List({ list }: Props) {
